69bae2ed3650a75d555bdf1a1d398b8f18e41867
Tanish013 <keshavdhingra0103@gmail.com>
2020-05-24 23:14:18 +0530

    web scrapper

---
+++ images.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+import requests
+from io import BytesIO
+from PIL import Image
+import os
+def start_search():
+    search = input("Search for: ")
+    params = {'q':search}
+    dir_name = search.replace(" ","_".lower())
+    if not os.path.isdir(dir_name):
+        os.makedirs(dir_name)
+
+    r = requests.get("http://www.bing.com/images/search",params=params)
+
+    soup = BeautifulSoup(r.text,"html.parser")
+    links = soup.findAll("a",{"class":"thumb"})
+
+    for item in links:
+        try:
+            img_obj = requests.get(item.attrs["href"])
+            print("Getting",item.attrs["href"])
+            title = item.attrs['href'].split("/")[-1]
+            try:
+                img = Image.open(BytesIO(img_obj.content))
+                img.save("./"+dir_name+"/"+ title, img.format)
+            except:
+                print("Could not save image")
+        except:
+            print("Could not request image")
+    start_search()
+
+start_search()
